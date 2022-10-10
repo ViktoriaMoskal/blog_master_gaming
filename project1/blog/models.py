b7d8e383af9f170dfa6d75a92016092b99bc5554
ViktoriaMoskal <moskalenkoviktoriya7@gmail.com>
2022-10-10 16:48:40 +0300

    images

--- project1/blog/models.py
+++ project1/blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.shortcuts import reverse
 
+
 class Post(models.Model):
-    title=models.CharField(max_length=150, db_index=True)
-    slug=models.SlugField(max_length=150, unique=True)
-    body=models.TextField(blank=True, db_index=True)
-    date_pub=models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
 
     def __str__(self):
         return self.title
@@ -14,10 +17,12 @@ class Post(models.Model):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
 
-
-class Tags(models.Model):
-    title=models.CharField(max_length=50, db_index=True)
-    slug=models.SlugField(max_length=50, unique=True)
+class Tag(models.Model):
+    title = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
